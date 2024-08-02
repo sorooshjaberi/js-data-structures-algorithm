036fa9a5a9ef2a54f26842466a5fa59236f42ec9
Soroush Jaberi <sorooshjaberi@gmail.com>
2024-08-03 02:08:34 +0330

    added queues

---
+++ linkedList/bidirectionalLinkedlist.ts
@@ -0,0 +1,175 @@
+class StackNode<T = unknown> {
+  prevNode: StackNode<T> | null;
+  nextNode: StackNode<T> | null;
+
+  constructor(public value: T) {
+    this.prevNode = this.nextNode = null;
+  }
+
+  setPrevNode(prevNode: StackNode<T> | null) {
+    this.prevNode = prevNode;
+  }
+
+  setNextNode(nextNode: StackNode<T> | null) {
+    this.nextNode = nextNode;
+  }
+}
+
+export class BidirectionalLinkedList<T = unknown> {
+  headNode: StackNode<T> | null;
+  tailNode: StackNode<T> | null;
+  length: number;
+  constructor(value?: T) {
+    this.headNode = this.tailNode = value ? new StackNode(value) : null;
+    this.length = value ? 1 : 0;
+  }
+
+  isEmpty() {
+    return !this.headNode;
+  }
+
+  isSingleValue() {
+    return this.headNode === this.tailNode;
+  }
+
+  popFirst() {
+    if (!this.isEmpty()) {
+      this.length--;
+      const headValue = this.headNode!.value;
+      this.headNode = this.headNode!.nextNode;
+
+      return headValue;
+    }
+  }
+
+  peekFirst() {
+    return this.headNode?.value;
+  }
+
+  push(value: T) {
+    this.length++;
+    const newNode = new StackNode(value);
+    if (this.isEmpty()) {
+      this.headNode = this.tailNode = newNode;
+      return;
+    }
+
+    this.tailNode!.setNextNode(newNode);
+    newNode.setPrevNode(this.tailNode);
+
+    this.tailNode = newNode;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    this.length--;
+    if (this.isSingleValue()) {
+      const lastNode = this.tailNode;
+
+      this.headNode = this.tailNode = null;
+
+      return lastNode?.value;
+    }
+
+    let iteratee = this.headNode;
+
+    while (iteratee) {
+      if (iteratee.nextNode === this.tailNode) {
+        const lastNode = iteratee.nextNode;
+        iteratee.nextNode = null;
+        lastNode!.prevNode = null;
+
+        this.tailNode = iteratee;
+
+        return lastNode?.value;
+      }
+
+      iteratee = iteratee.nextNode;
+    }
+  }
+
+  peek() {
+    if (this.isSingleValue()) {
+      const lastNode = this.tailNode;
+
+      return lastNode?.value;
+    }
+
+    let iteratee = this.headNode;
+
+    while (iteratee) {
+      if (iteratee.nextNode === this.tailNode) {
+        const lastNode = iteratee.nextNode;
+
+        return lastNode?.value;
+      }
+
+      iteratee = iteratee.nextNode;
+    }
+  }
+
+  toArray() {
+    let arr: T[] = [];
+
+    let iteratee = this.headNode;
+
+    while (iteratee) {
+      arr.push(iteratee.value);
+      iteratee = iteratee.nextNode;
+    }
+
+    return arr;
+  }
+}
+function reverseString(str: string) {
+  const stack = new BidirectionalLinkedList<string>();
+
+  Array.from(str).forEach((char) => stack.push(char));
+
+  let newStr = "";
+
+  let char = stack.pop();
+
+  while (char) {
+    newStr += char;
+    char = stack.pop();
+  }
+
+  return newStr;
+}
+
+function getIsStringBalance(str: string) {
+  const openingChars = ["[", "<", "("];
+  const closingChars = ["]", ">", ")"];
+
+  const stack = new BidirectionalLinkedList<string>();
+  const chars = Array.from(str);
+
+  for (let i = 0; i <= chars.length; i++) {
+    const char = chars[i];
+
+    if (openingChars.includes(char)) {
+      stack.push(char);
+    } else if (closingChars.includes(char)) {
+      const lastChar = stack.pop();
+
+      if (
+        !(
+          lastChar &&
+          openingChars.indexOf(lastChar) === closingChars.indexOf(char)
+        )
+      ) {
+        return false;
+      }
+    }
+  }
+
+  if (stack.peek() && openingChars.includes(stack.peek() || "")) {
+    return false;
+  }
+
+  return true;
+}
---
+++ queues/index.ts
@@ -0,0 +1,45 @@
+import { BidirectionalLinkedList } from "../linkedList/bidirectionalLinkedlist";
+
+class Queue<T = unknown> {
+  private _linkedList: BidirectionalLinkedList<T>;
+  constructor(private cap?: number) {
+    this._linkedList = new BidirectionalLinkedList();
+  }
+
+  enqueue(value: T) {
+    if (this.cap !== undefined && this._linkedList.length === this.cap) {
+      return;
+    }
+    this._linkedList.push(value);
+  }
+
+  toArray() {
+    return this._linkedList.toArray();
+  }
+
+  dequeue() {
+    return this._linkedList.popFirst();
+  }
+
+  peek() {
+    return this._linkedList.peekFirst();
+  }
+
+  isEmpty() {
+    return this._linkedList.isEmpty();
+  }
+
+  isFull() {
+    return this.cap !== undefined && this._linkedList.length === this.cap;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(5);
+queue.dequeue();
+
+console.log(queue.toArray());
